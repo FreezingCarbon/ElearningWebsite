f6e9d3adb7040648eb4700e51cae5d2e7f6e16e4
Saegusa <20162230@student.hust.edu.vn>
2019-04-23 00:46:19 +0700

    update migrations and database

---
+++ ElearningWebsite.API/Migrations/20190422165835_Remove_Unecessary_Column.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElearningWebsite.API.Migrations
+{
+    public partial class Remove_Unecessary_Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Budget",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Fee",
+                table: "Courses");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Budget",
+                table: "Students",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fee",
+                table: "Courses",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
