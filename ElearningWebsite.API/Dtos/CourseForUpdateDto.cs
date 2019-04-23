b918018e4aa97c4fa49fed038f90d77eea708ab1
Saegusa <20162230@student.hust.edu.vn>
2019-04-23 11:39:11 +0700

    Create CourseForUpdateDto.cs

---
+++ ElearningWebsite.API/Dtos/CourseForUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace ElearningWebsite.API.Dtos
+{
+    public class CourseForUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Requirement { get; set; }
+    }
+}
