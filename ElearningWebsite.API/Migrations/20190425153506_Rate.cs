44220d2d3a16c27709e1ae00c315d133fb4b5549
Saegusa <20162230@student.hust.edu.vn>
2019-04-26 22:02:18 +0700

    Database add Rating in StudentCourses table

---
+++ ElearningWebsite.API/Migrations/20190425153506_Rate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElearningWebsite.API.Migrations
+{
+    public partial class Rate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rate",
+                table: "StudentCourses",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "StudentCourses");
+        }
+    }
+}
