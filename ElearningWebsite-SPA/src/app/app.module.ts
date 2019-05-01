be6cf9535ba37fcfef2619b59d12ff013930254f
Saegusa <20162230@student.hust.edu.vn>
2019-05-01 10:23:43 +0700

    add new component and provider into app

--- ElearningWebsite-SPA/src/app/app.module.ts
+++ ElearningWebsite-SPA/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
+import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      HomeComponent,
+      CoursesComponent,
+      AboutComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      NgbModule
+   ],
+   providers: [
+      AuthService,
+      ErrorInterceptorProvider
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
