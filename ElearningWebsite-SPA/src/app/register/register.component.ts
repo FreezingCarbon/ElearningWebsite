a5d8c99545ac4a33ed50b1d80ae6548c4ac5a01b
Saegusa <20162230@student.hust.edu.vn>
2019-05-01 10:21:49 +0700

    complete Register method

---
+++ ElearningWebsite-SPA/src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output,  EventEmitter, Input } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() isTeacher: boolean;
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model, this.isTeacher).subscribe(next => {
+      console.log('register success');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+
+}
