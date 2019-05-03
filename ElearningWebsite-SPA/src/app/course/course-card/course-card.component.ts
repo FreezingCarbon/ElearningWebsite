daf73e66b779dd9477056fbea0085c6ace30373a
Saegusa <20162230@student.hust.edu.vn>
2019-05-03 20:53:59 +0700

    course detail view ( except teacher )

---
+++ ElearningWebsite-SPA/src/app/course/course-card/course-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Course } from 'src/app/_models/course';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+  @Input() course: Course;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+}
