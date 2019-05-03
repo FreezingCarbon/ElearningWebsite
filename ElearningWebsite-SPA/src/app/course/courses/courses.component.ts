05d10317132d92ff0f2861774595e4abb427eee7
Saegusa <20162230@student.hust.edu.vn>
2019-05-03 20:52:48 +0700

    all course view

---
+++ ElearningWebsite-SPA/src/app/course/courses/courses.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../../_models/course';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  courses: Course[];
+  pagination: Pagination;
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.courses = data['courses'].result;
+      this.pagination = data['courses'].pagination;
+    });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadCourses();
+  }
+
+  loadCourses() {
+    this.userService.getCourses(this.pagination.currentPage, this.pagination.itemsPerPage).subscribe((res: PaginatedResult<Course[]>) => {
+      this.courses = res.result;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+}
