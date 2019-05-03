3e8ad8519c7ff721cfbb0e8bfba32e1012b31792
Saegusa <20162230@student.hust.edu.vn>
2019-05-03 20:55:32 +0700

    update routing path

--- ElearningWebsite-SPA/src/app/routes.ts
+++ ElearningWebsite-SPA/src/app/routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CoursesComponent } from './courses/courses.component';
+import { CoursesComponent } from './course/courses/courses.component';
 import { AboutComponent } from './about/about.component';
-import { CourseslistComponent } from './courseslist/courseslist.component';
-import { AuthGuard } from './_guard/auth.guard';
+import { CoursesResolver } from './_resolvers/courses.resolver';
+import { CourseNDetailComponent } from './course/course-n-detail/course-n-detail.component';
+import { CourseNResolver } from './_resolvers/course-n.resolver';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'courses', component: CoursesComponent},
-    { path: 'courseslist', component: CourseslistComponent, canActivate: [AuthGuard]},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        children:[
+            { path: 'courses', component: CoursesComponent, resolve: {courses: CoursesResolver} },
+            { path: 'courses/:id', component: CourseNDetailComponent, resolve: {course: CourseNResolver} }
+        ]
+    },
+    { path: 'student/register/:registerMode/:isTeacher', component: HomeComponent},
     { path: 'about', component: AboutComponent},
     { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
