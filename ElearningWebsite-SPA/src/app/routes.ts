a233097f7405ee8ccf0dc72b6dbd4bffc5e06a01
Saegusa <20162230@student.hust.edu.vn>
2019-05-04 23:04:16 +0700

    update new route link

--- ElearningWebsite-SPA/src/app/routes.ts
+++ ElearningWebsite-SPA/src/app/routes.ts
@@ -5,15 +5,29 @@ import { AboutComponent } from './about/about.component';
 import { CoursesResolver } from './_resolvers/courses.resolver';
 import { CourseNDetailComponent } from './course/course-n-detail/course-n-detail.component';
 import { CourseNResolver } from './_resolvers/course-n.resolver';
+import { TCoursesResolver } from './_resolvers/t-courses.resolver';
+import { TeacherAuthGuard } from './_guard/teacher-auth.guard';
+import { CourseEditComponent } from './course/course-edit/course-edit.component';
+import { EditCourseResolver } from './_resolvers/edit-course.resolver';
+import { PreventUsavedChanges } from './_guard/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
     {
         path: '',
         runGuardsAndResolvers: 'always',
-        children:[
+        children: [
             { path: 'courses', component: CoursesComponent, resolve: {courses: CoursesResolver} },
             { path: 'courses/:id', component: CourseNDetailComponent, resolve: {course: CourseNResolver} }
+        ]
+    },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [TeacherAuthGuard],
+        children: [
+            { path: 'teacher/courses', component: CoursesComponent, resolve: {courses: TCoursesResolver} },
+            { path: 'teacher/courses/edit/:id', component: CourseEditComponent, canDeactivate: [PreventUsavedChanges] , resolve: {course: EditCourseResolver} },
         ]
     },
     { path: 'student/register/:registerMode/:isTeacher', component: HomeComponent},
