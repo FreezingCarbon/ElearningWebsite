e8d8181ade1686d72e8651571ca9ef8968f45a60
Saegusa <20162230@student.hust.edu.vn>
2019-05-10 00:12:00 +0700

    add guard

--- ElearningWebsite-SPA/src/app/_guard/student-auth.guard.ts
+++ ElearningWebsite-SPA/src/app/_guard/student-auth.guard.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TeacherAuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router, private alertify: AlertifyService) {}
-
+export class StudentAuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) {}
   canActivate(): boolean {
     const data = JSON.parse(localStorage.getItem('user'));
     if (data) {
-      if (this.authService.loggedIn() && data['role'] === 'Teacher') {
+      if (this.authService.loggedIn() && data['role'] === 'Student') {
         return true;
       }
     }
