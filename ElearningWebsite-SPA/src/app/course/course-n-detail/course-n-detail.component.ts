8624c15cc6594c1ac407d9403d9aa0d6eaa80570
Saegusa <20162230@student.hust.edu.vn>
2019-05-09 21:53:06 +0700

    update course detail view

--- ElearningWebsite-SPA/src/app/course/course-n-detail/course-n-detail.component.ts
+++ ElearningWebsite-SPA/src/app/course/course-n-detail/course-n-detail.component.ts
@@ -5,6 +5,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 import { Course } from 'src/app/_models/course';
 import { routerNgProbeToken } from '@angular/router/src/router_module';
 import { AuthService } from 'src/app/_services/auth.service';
+import { Video } from 'src/app/_models/video';
 
 @Component({
   selector: 'app-course-n-detail',
@@ -13,6 +14,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class CourseNDetailComponent implements OnInit {
   course: Course;
+  videos: Video[];
   requirements: string[];
   descriptions: string[];
 
@@ -23,8 +25,13 @@ export class CourseNDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.course = data['course'];
     });
-    this.requirements = this.course.requirement.split('\n');
-    this.descriptions = this.course.description.split('\n');
+    if(this.course.requirement) {
+      this.requirements = this.course.requirement.split('\n');
+    }
+    if(this.course.description) {
+      this.descriptions = this.course.description.split('\n');
+    }
+    this.videos = this.course.videos;
   }
 
   toRegister() {
@@ -33,7 +40,7 @@ export class CourseNDetailComponent implements OnInit {
 
   isTeacher() {
     const data = JSON.parse(localStorage.getItem('user'));
-    if(data) {
+    if (data) {
       return this.authService.loggedIn() && data['role'] === 'Teacher';
     } else {
       return false;
@@ -42,12 +49,16 @@ export class CourseNDetailComponent implements OnInit {
 
   loggedIn() {
     const data = JSON.parse(localStorage.getItem('user'));
-    if(data) {
-      return this.authService.loggedIn(); 
+    if (data) {
+      return this.authService.loggedIn();
     }
   }
 
   toEdit() {
-    this.router.navigate(['teacher/courses/edit/', this.course.courseId]);
+    this.router.navigate(['teacher/courses/edit/', this.course.courseId, true]);
+  }
+
+  videoPlayTab(link: string) {
+    window.open(link, '_blank');
   }
 }
