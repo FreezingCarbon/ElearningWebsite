1cf5b52b0e57872f9e7a4ac10d5b696d2b228239
Saegusa <20162230@student.hust.edu.vn>
2019-05-09 21:52:02 +0700

    update course card view

--- ElearningWebsite-SPA/src/app/course/course-card/course-card.component.ts
+++ ElearningWebsite-SPA/src/app/course/course-card/course-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Course } from 'src/app/_models/course';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-course-card',
@@ -10,8 +11,21 @@ import { Router } from '@angular/router';
 export class CourseCardComponent implements OnInit {
   @Input() course: Course;
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  isTeacher() {
+    const data = JSON.parse(localStorage.getItem('user'));
+    if(data) {
+      return this.authService.loggedIn() && data['role'] == 'Teacher';
+    } else { 
+      return false; 
+    }
+  }
+
+  toTeacherCourse(courseId) {
+    this.router.navigate(['/teacher/courses', courseId])
   }
 }
